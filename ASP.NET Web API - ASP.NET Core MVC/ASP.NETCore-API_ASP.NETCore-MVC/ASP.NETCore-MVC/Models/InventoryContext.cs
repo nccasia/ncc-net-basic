a970b7ca874bb1c2c0c396147c09dca5d4b3c2d8
vuhoang1it <vuhoang1it@gmail.com>
2021-01-06 14:39:40 +0700

    Sip-37 Update ASP API MVC Document

---
+++ ASP.NET Web API - ASP.NET Core MVC/ASP.NETCore-API_ASP.NETCore-MVC/ASP.NETCore-MVC/Models/InventoryContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASP.NETCore_MVC.Models
+{
+    public partial class InventoryContext : DbContext
+    {
+        public InventoryContext()
+        {
+        }
+
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId)
+                    .HasName("PK__Products__B40CC6CDAC8423C7");
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Color)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
