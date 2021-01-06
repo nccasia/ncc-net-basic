84a83ddcdf9ab9c4b66854d564dc1c97bef8d2f9
vuhoang1it <vuhoang1it@gmail.com>
2021-01-06 11:12:58 +0700

    Update ASP.Net Core API document

---
+++ ASP.NET Web API - ASP.NET Core MVC/ASP.NETCore-API_ASP.NETCore-MVC/ASP.NETCore-API/Controllers/TokenController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using ASP.NETCore_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ASP.NETCore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        public UserInfo _currentUser;
+        public TokenController(IConfiguration config)
+        {
+            _configuration = config;
+            _currentUser = new UserInfo()
+            {
+                Email = "foo@gmail.com",
+                FirstName = "foo",
+                LastName = "bar",
+                Password = "password",
+                UserId = 1,
+                UserName = "foo bar",
+                CreatedDate = DateTime.Now
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserInfo _userData)
+        {
+
+            if (_userData != null && _userData.Email != null && _userData.Password != null)
+            {
+                var user = GetUser(_userData.Email, _userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("Id", user.UserId.ToString()),
+                    new Claim("FirstName", user.FirstName),
+                    new Claim("LastName", user.LastName),
+                    new Claim("UserName", user.UserName),
+                    new Claim("Email", user.Email)
+                   };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims, expires: DateTime.UtcNow.AddDays(1), signingCredentials: signIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private UserInfo GetUser(string email, string password)
+        {
+            if (_currentUser.Email != email || _currentUser.Password != password)
+                return null;
+            return _currentUser;
+        }
+    }
+}
